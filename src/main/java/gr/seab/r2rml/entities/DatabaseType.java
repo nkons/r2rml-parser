8936386657554eb44e54245c94f6d8c23e32736c
Nikolaos Konstantinou <nkons@live.com>
2014-07-01 10:28:10 +0300

    small fixes

--- src/main/java/gr/seab/r2rml/entities/DatabaseType.java
+++ src/main/java/gr/seab/r2rml/entities/DatabaseType.java
@@ -1,22 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported 
+ * License (the "License"). You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ * 
+ *  http://creativecommons.org/licenses/by-nc/3.0/
+ *  
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
  */
-
 package gr.seab.r2rml.entities;
 
 /**
  *
+ * Source database types
  * @author nkons
+ * 
  */
 public enum DatabaseType {
-        
-        MYSQL,
+	
+	MYSQL,
 	
 	POSTGRESQL,
 	
 	ORACLE,
 	
 	OTHER;
+	
 }
