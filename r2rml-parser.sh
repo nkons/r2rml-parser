740017971b36bca6aabc9935d95fbfbb94340f95
Nikolaos Konstantinou <nkons@live.com>
2014-07-21 18:18:42 +0300

    server should run in online mode

--- r2rml-parser.sh
+++ r2rml-parser.sh
@@ -1,3 +1,3 @@
 echo "This is R2RML Parser 0.6-alpha"
-java -Xms128m -Xmx1024m -cp "./*;./lib/*;" -jar lib/r2rml-parser-0.6.jar
+java -Xms128m -Xmx1024m -cp "./*;./lib/*;" -jar lib/r2rml-parser-0.6.jar -o true
 echo "R2RML Parser 0.6-alpha. Done."
