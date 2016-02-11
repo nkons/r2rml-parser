72adde67d9986ad091ab00b5b1faebd63c988e2f
Nikolaos Konstantinou <nkons@live.com>
2016-02-11 16:31:11 +0000

    version 0.8-alpha

--- r2rml-parser.bat
+++ r2rml-parser.bat
@@ -1,4 +1,4 @@
 @echo off
-echo This is R2RML Parser 0.7-alpha. Run with -h for help on options.
+echo This is R2RML Parser 0.8-alpha. Run with -h for help on options.
 java -Xms128m -Xmx1024m -cp "./*;./lib/*;" gr.seab.r2rml.beans.Main %1 %2
-echo R2RML Parser 0.7-alpha. Done.
+echo R2RML Parser 0.8-alpha. Done.
--- r2rml-parser.sh
+++ r2rml-parser.sh
@@ -1,3 +1,3 @@
-echo "This is R2RML Parser 0.7-alpha. Run with -h for help on options."
-java -Xms128m -Xmx1024m -cp "./*;./lib/*;" -jar lib/r2rml-parser-0.7.jar $1 $2
-echo "R2RML Parser 0.7-alpha. Done."
+echo "This is R2RML Parser 0.8-alpha. Run with -h for help on options."
+java -Xms128m -Xmx1024m -cp "./*;./lib/*;" -jar lib/r2rml-parser-0.8.jar $1 $2
+echo "R2RML Parser 0.8-alpha. Done."
